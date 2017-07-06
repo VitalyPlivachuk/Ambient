8b1a0840240e42e0acf85478893d06cd53f449d3
VitalyPlivachuk <vitaly.plivachuk@gmail.com>
2017-07-06 13:28:17 +0300

    Initial Commit

---
+++ Ambient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ambient
+//
+//  Created by Vitaly Plivachuk on 06.07.17.
+//  Copyright © 2017 Vitaly Plivachuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
